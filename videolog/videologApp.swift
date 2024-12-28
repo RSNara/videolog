37b0b74e8f73faa274e5ea1a8ea59c66af7922d4
Ramanpreet Nara <ramanpreetnara@gmail.com>
2024-12-27 17:09:12 -0800

    Initial Commit

---
+++ videolog/videologApp.swift
@@ -0,0 +1,17 @@
+//
+//  videologApp.swift
+//  videolog
+//
+//  Created by Ramanpreet Nara on 12/27/24.
+//
+
+import SwiftUI
+
+@main
+struct videologApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ videologTests/videologTests.swift
@@ -0,0 +1,17 @@
+//
+//  videologTests.swift
+//  videologTests
+//
+//  Created by Ramanpreet Nara on 12/27/24.
+//
+
+import Testing
+@testable import videolog
+
+struct videologTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
